59b815746b0b7b1447b00acbfc55f7112d39d2c1
Daniel Gama <danielgmbr30@gmail.com>
2026-08-11 09:33:13 -0300

    feat: while

--- 1-bimestre/pratica/Treinamento-Java/src/condicionais/EstudoCondicionais.java
+++ 1-bimestre/pratica/Treinamento-Java/src/condicionais/EstudoCondicionais.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class EstudoCondicionais {
     public static void main(String[] args) {
         Scanner entrada_usuario = new Scanner(System.in);
-        //k
+        int indice = 0;
         String[] feira = {"Banana", "Manga", "Abobora"};
         int[] valor = {10, 15, 8};
 
         System.out.println("O preço da " + feira[0] + " é R$" + valor[0]);
+
+        while (indice < feira.length){
+            System.out.println("Isso vai se repetir ate " + (indice + 1) + " ser igual a 3");
+            indice++;
+        }
     }
 }
