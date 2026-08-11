df906334fb8e30d2ab5cf268496076a418dae948
Daniel Gama <danielgmbr30@gmail.com>
2026-08-11 17:29:42 -0300

    feat: terminando capitulo metodos

---
+++ 1-bimestre/pratica/Treinamento-Java/src/aula1/main.java
@@ -0,0 +1,7 @@
+package aula1;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 1-bimestre/pratica/Treinamento-Java/src/metodos/ex1.java
@@ -0,0 +1,25 @@
+package metodos;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 10;
+
+        beginningToMiddle(first, second);
+
+        System.out.println(first);
+    }
+
+    public static void beginningToMiddle (int start, int end) {
+        int middle = (start + end)/2;
+        while (start < middle) {
+            System.out.println("step");
+            start++;
+        }
+    }
+}
+
+
+
